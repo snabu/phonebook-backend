532746cff270e8a532f8bf52a33fb82d69513171
samuli <samuli.tuoriniemi@yahoo.com>
2018-04-09 18:24:48 +0300

    Initiate app

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/info', (req, res) => {
     res.send('<p>Puhelinluettelossa ' + persons.length + ' henkilön tiedot<p>' + new Date())
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
