917c405706f7de1f234a0984ef89a7da6997646e
unknown <samuli.tuoriniemi@yahoo.com>
2018-10-19 22:14:52 +0300

    exercises 13-18, pt3

--- index.js
+++ index.js
@@ -3,28 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Martti Tienari",
-        "number": "040-123456",
-        "id": 2
-    },
-    {
-        "name": "Arto Järvinen",
-        "number": "040-123456",
-        "id": 3
-    },
-    {
-        "name": "Lea Kutvonen",
-        "number": "040-123456",
-        "id": 4
-    }
-]
+const Person = require('./models/person.js')
 
 
 app.use(express.static('build'))
@@ -35,7 +14,16 @@ morgan.token('payload', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :payload :status :res[content-length] - :response-time ms'))
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+
+    Person
+        .find({})
+        .then(persons => {
+            console.log("puhelinluettelo:")
+            persons.map(person => {
+                console.log(Person.format(person))
+            })
+            res.json(persons.map(Person.format))
+        })
 })
 
 
@@ -43,40 +31,85 @@ app.post('/api/persons', (req, res) => {
     if (req.body === undefined) {
         return res.status(400).json({error: 'content missing'})
     }
-    const person = req.body
-    if (!person.name ) {
+    if (!req.body.name ) {
         return res.status(400).json({error: 'person.name missing'})
     }
-    if (!person.number ) {
+    if (!req.body.number ) {
+        return res.status(400).json({error: 'person.number missing'})
+    }
+    const person = new Person({
+        name : req.body.name,
+        number : req.body.number
+    })
+
+    Person.find({name : req.body.name})
+        .then(persons => {
+            if (persons.length > 0)
+                return res.status(409).send()
+            person
+                .save()
+                .then(savedPerson => {
+                    res.status(201).json(Person.format(savedPerson))
+                })
+                .catch(error => {
+                    console.log(error)
+                    res.status(500).send()
+                })
+
+        })
+
+
+})
+
+
+
+app.put('/api/persons/:id', (req, res) => {
+    if (req.body === undefined) {
+        return res.status(400).json({error: 'content missing'})
+    }
+    if (!req.body.name ) {
+        return res.status(400).json({error: 'person.name missing'})
+    }
+    if (!req.body.number ) {
         return res.status(400).json({error: 'person.number missing'})
     }
 
-    const index = persons.findIndex(function(person) {
-        return person.name.toLowerCase() === name.toLowerCase()
-    }, name=person.name)
+    Person.findByIdAndUpdate(req.params.id, { $set: { number: req.body.number }}, { new: true })
+        .then(person => {
+            res.status(200).json(person)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).send()
+        })
 
-    if (index > -1) {
-        return res.status(409).json({error : 'person with name ' + person.name + ' already exists'})
-    }
-    person.id = Math.floor(Math.random() * Math.floor(100000000));
-    persons = persons.concat(person)
-    res.status(201).json(person)
 })
 
+
 app.get('/api/persons/:id', (req, res) => {
-    const person = persons.find(person =>{
-        return person.id.toString() === req.params.id
-    } )
-    person ? res.json(person) : res.status(404).send({error : "no entry with id " + req.params.id })
+    Person.findById(req.params.id)
+        .then(person => {
+            res.json(Person.format(person))
+        })
+        .catch(err => {
+            res.status(404).send({error : "no entry with id " + req.params.id })
+        })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    persons = persons.filter(person => person.id.toString() != req.params.id)
-    res.status(204).end()
+    Person
+        .findOneAndDelete({_id: req.params.id})
+        .then(resp => {
+            res.status(204).end()
+        })
 })
 
 app.get('/info', (req, res) => {
-    res.send('<p>Puhelinluettelossa ' + persons.length + ' henkilön tiedot<p>' + new Date())
+    Person.count({})
+        .then(result => {
+            res.send('<p>Puhelinluettelossa ' + result + ' henkilön tiedot<p>' + new Date())
+        })
+
 })
 
 const PORT = process.env.PORT || 3001
---
+++ models/person.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://' + process.env.FULLSTACKOPEN_MLAB_USER + ':' + process.env.FULLSTACKOPEN_MLAB_PWD + '@ds239309.mlab.com:39309/fullstackopen'
+
+
+mongoose.connect(url)
+
+const personSchema = mongoose.model('Person', {
+    name: String,
+    number: String
+})
+
+personSchema.format = function(person) {
+    return {
+        name : person.name,
+        number : person.number,
+        id : person._id
+    }
+}
+module.exports = personSchema
